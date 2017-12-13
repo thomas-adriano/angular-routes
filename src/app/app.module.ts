cdceb09b098a390164fc0ab91d6ed6cc6a5ef7d5
Thomas Oelke Adriano <thomas@thomas-mpb.home>
2017-12-13 01:09:18 -0200

    app-routing.module moved one dir up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing/app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { DashboardModule } from './dashboard/dashboard.module';
 import { NotFoundComponent } from './not-found/not-found.component';
 
